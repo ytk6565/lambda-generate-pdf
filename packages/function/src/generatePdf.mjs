1f9867ebf54c80722eb172e7cfdb3ac6d43221b7
ytk6565 <ytk6565@gmail.com>
2024-12-02 06:52:39 +0900

    fix

--- packages/function/src/generatePdf.mjs
+++ packages/function/src/generatePdf.mjs
@@ -33,7 +33,7 @@ const generatePdf = async () => {
   const page = await browser.newPage();
 
   // ダッシュボードにアクセスする
-  await page.goto("http://localhost:3000/document?message=HelloWorld", {
+  await page.goto("http://localhost:3000/document?message=こんにちは", {
     waitUntil: "networkidle0",
   });
 
