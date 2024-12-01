a55a01dc1db09d4b98e7be4b3d0fed9718954946
ytk6565 <ytk6565@gmail.com>
2024-12-01 23:37:51 +0900

    first commit

---
+++ packages/app/src/pages/document.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { useRoute } from "#vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <p>{{ route.query.message }} from query.</p>
+  </div>
+</template>
