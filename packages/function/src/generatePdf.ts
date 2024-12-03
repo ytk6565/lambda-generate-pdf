1f9d815288425d492a9b980643a0b52f950744d6
ytk6565 <ytk6565@gmail.com>
2024-12-03 19:25:59 +0900

    test

--- packages/function/src/generatePdf.ts
+++ packages/function/src/generatePdf.ts
@@ -33,7 +33,7 @@ const generatePdf = async () => {
   const page = await browser.newPage();
 
   // ダッシュボードにアクセスする
-  await page.goto("http://localhost:3000/document?message=こんにちは", {
+  await page.goto("http://localhost:3000/document?message=こんばんは", {
     waitUntil: "networkidle0",
   });
 
