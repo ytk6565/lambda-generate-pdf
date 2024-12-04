a0a23f6f84a797a0ece849f4d604b426efbb59b0
ytk6565 <ytk6565@gmail.com>
2024-12-04 13:18:49 +0900

    fix esbuild

---
+++ packages/function/esbuild.config.mjs
@@ -0,0 +1,38 @@
+import { build } from "esbuild";
+import path from "node:path";
+
+/** @type {import('esbuild').Plugin} */
+const jsToMjs = {
+  name: "js-to-mjs",
+  setup(build) {
+    // Find all relative imports (starting with ".") that ends with ".js" and
+    // replace the file extension with ".mjs"
+    build.onResolve({ filter: /^..+\.js$/ }, (args) => {
+      return {
+        path: path.join(args.resolveDir, args.path.slice(0, -3) + ".mjs"),
+      };
+    });
+  },
+};
+
+/** @type {import('esbuild').BuildOptions} */
+const options = {
+  platform: "node",
+  format: "esm",
+  target: "es2020",
+  outdir: "dist",
+  packages: "external",
+  entryPoints: [
+    "index.ts",
+    "worker.ts",
+    "src/generatePdf.ts",
+    "src/uploadResultFilesToS3.ts",
+  ],
+  bundle: false,
+  plugins: [jsToMjs],
+};
+
+build(options).catch((err) => {
+  process.stderr.write(err.stderr);
+  process.exit(1);
+});
