78a2b69a38d8fdc26c4778c6232c8c1b886ceb23
ytk6565 <ytk6565@gmail.com>
2024-12-05 19:23:04 +0900

    不要な処理を削除

--- packages/function/esbuild.config.mjs
+++ packages/function/esbuild.config.mjs
@@ -1,18 +1,4 @@
 import { build } from "esbuild";
-import path from "node:path";
-
-/** @type {import('esbuild').Plugin} */
-const jsToMjs = {
-  name: "js-to-mjs",
-  setup(build) {
-    // src を含むパスの拡張子を .mjs に変更する
-    build.onResolve({ filter: /.*\/src\/.*\.ts/ }, (args) => {
-      return {
-        path: path.join(args.resolveDir, args.path + ".mjs"),
-      };
-    });
-  },
-};
 
 /** @type {import('esbuild').BuildOptions} */
 const options = {
@@ -23,9 +9,7 @@ const options = {
   outExtension: { ".js": ".mjs" },
   packages: "external",
   entryPoints: ["index.ts"],
-  packages: "external",
   bundle: true,
-  plugins: [jsToMjs],
 };
 
 build(options).catch((err) => {
