5159993fa9c4cc4a269a4e76573da1506f79d4bc
ytk6565 <ytk6565@gmail.com>
2024-12-06 13:48:37 +0900

    qpdf を実行する

--- packages/function/src/generatePdf.ts
+++ packages/function/src/generatePdf.ts
@@ -1,10 +1,13 @@
 import type { Browser } from "puppeteer-core";
 
+import { promisify } from "node:util";
 import { promises } from "node:fs";
 import { exec } from "node:child_process";
 
 const OUTPUT_PDF_PATH = "/tmp/output.pdf";
 const OUTPUT_ENCRYPTED_PDF_PATH = "/tmp/output-encrypted.pdf";
+
+const promisifiedExec = promisify(exec);
 
 /**
  * PDF生成
@@ -26,19 +29,8 @@ export const generatePdfFactory = (browser: Browser) => async (url: string) => {
     path: OUTPUT_PDF_PATH,
   });
 
-  exec(
+  await promisifiedExec(
     `qpdf --encrypt "" ownerpass 256 --print=none --modify=none --extract=n --annotate=n -- ${OUTPUT_PDF_PATH} ${OUTPUT_ENCRYPTED_PDF_PATH}`,
-    (error, stdout: string, stderr: string) => {
-      if (error) {
-        console.error(`エラーが発生しました: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`エラーが発生しました: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    }
   );
 
   console.log("PDFが生成されました");
@@ -48,7 +40,7 @@ export const generatePdfFactory = (browser: Browser) => async (url: string) => {
   await browser.close();
 
   // PDFバッファを返す
-  const pdfBuffer = await promises.readFile(OUTPUT_ENCRYPTED_PDF_PATH);
+  const pdfBuffer = promises.readFile(OUTPUT_ENCRYPTED_PDF_PATH);
 
   return pdfBuffer;
 };
