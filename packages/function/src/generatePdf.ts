de7dd6a6d35677088257e3a9f0ddacab2e98c736
ytk6565 <ytk6565@gmail.com>
2024-12-08 14:56:57 +0900

    Revert "pdfkit"
    This reverts commit c4d0fe4ca269c0075376849ad294e9102e43b768.

--- packages/function/src/generatePdf.ts
+++ packages/function/src/generatePdf.ts
@@ -3,69 +3,68 @@ import type { Browser } from "puppeteer-core";
 import { execFile } from "node:child_process";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
-import { promises, createWriteStream } from "node:fs";
+import { promises } from "node:fs";
 import { promisify } from "node:util";
 
-// import PDFDocument from "pdf-lib";
-import PDFDocument from "pdfkit";
+import { PDFDocument } from "pdf-lib";
 
-// type Metadata = {
-//   title: string;
-//   author: string;
-//   subject: string;
-//   keywords: string[];
-//   producer: string;
-//   creator: string;
-//   creationDate: Date;
-//   modificationDate: Date;
-// };
+type Metadata = {
+  title: string;
+  author: string;
+  subject: string;
+  keywords: string[];
+  producer: string;
+  creator: string;
+  creationDate: Date;
+  modificationDate: Date;
+};
 
 const PAGE_NAVIGATION_TIMEOUT = 3000; // 3秒
 const PDF_GENERATION_TIMEOUT = 10000; // 10秒
 
-// const outputPdfPath = join(tmpdir(), "output.pdf");
+const outputPdfPath = join(tmpdir(), "output.pdf");
 const outputEncryptedPdfPath = join(tmpdir(), "output-encrypted.pdf");
 
 const promisifiedExecFile = promisify(execFile);
 
-// const setMetadata = async (
-//   buffer: Buffer,
-//   metadata: Metadata
-// ): Promise<Uint8Array> => {
-//   const pdfDoc = await PDFDocument.load(buffer);
+const setMetadata = async (
+  buffer: Buffer,
+  metadata: Metadata
+): Promise<Uint8Array> => {
+  const pdfDoc = await PDFDocument.load(buffer);
 
-//   pdfDoc.setTitle(metadata.title);
-//   pdfDoc.setAuthor(metadata.author);
-//   pdfDoc.setSubject(metadata.subject);
-//   pdfDoc.setKeywords(metadata.keywords);
-//   pdfDoc.setProducer(metadata.producer);
-//   pdfDoc.setCreator(metadata.creator);
-//   pdfDoc.setCreationDate(metadata.creationDate);
-//   pdfDoc.setModificationDate(metadata.modificationDate);
+  pdfDoc.setTitle(metadata.title);
+  pdfDoc.setAuthor(metadata.author);
+  pdfDoc.setSubject(metadata.subject);
+  pdfDoc.setKeywords(metadata.keywords);
+  pdfDoc.setProducer(metadata.producer);
+  pdfDoc.setCreator(metadata.creator);
+  pdfDoc.setCreationDate(metadata.creationDate);
+  pdfDoc.setModificationDate(metadata.modificationDate);
 
-//   return await pdfDoc.save();
-// };
+  return await pdfDoc.save();
+};
 
-// /**
-//  * PDF暗号化
-//  * @param inputPath 入力ファイルパス
-//  * @param outputPath 出力ファイルパス
-//  * @param ownerPassword オーナーパスワード
-//  */
-// async function encryptPdf(
-//   inputPath: string,
-//   outputPath: string,
-//   ownerPassword: string
-// ): Promise<void> {
-//   try {
-//     const args = ["encrypt", `--opw`, ownerPassword, inputPath, outputPath];
+/**
+ * PDF暗号化
+ * @param inputPath 入力ファイルパス
+ * @param outputPath 出力ファイルパス
+ * @param ownerPassword オーナーパスワード
+ */
+async function encryptPdf(
+  inputPath: string,
+  outputPath: string,
+  ownerPassword: string
+): Promise<void> {
+  try {
+    const args = ["encrypt", `--opw`, ownerPassword, inputPath, outputPath];
 
-//     await promisifiedExecFile("pdfcpu", args);
-//   } catch (error) {
-//     console.error("PDF暗号化中にエラーが発生しました:", error);
-//     throw new Error("PDF暗号化に失敗しました");
-//   }
-// }
+    await promisifiedExecFile("pdfcpu", args);
+  } catch (error) {
+    console.error("PDF暗号化中にエラーが発生しました:", error);
+    throw new Error("PDF暗号化に失敗しました");
+  }
+}
 
 /**
  * PDF生成
@@ -88,54 +87,25 @@ export const generatePdfFactory = (browser: Browser) => async (url: string) => {
       timeout: PDF_GENERATION_TIMEOUT,
     });
 
-    const doc = new PDFDocument({
-      userPassword: "",
-      ownerPassword: "owner",
-      permissions: {
-        modifying: false,
-        copying: false,
-        annotating: false,
-        fillingForms: false,
-        contentAccessibility: false,
-        documentAssembly: false,
-        // printing: "lowResolution" | "highResolution" | undefined;
-      },
-      info: {
-        Title: "My Document",
-        Author: "Me",
-        Subject: "Testing PDF encryption",
-        Keywords: "pdfkit, pdf encryption",
-        Creator: "pdfkit",
-        ModDate: new Date(),
-        CreationDate: new Date(),
-      },
+    // PDFメタデータを設定
+    const pdfWithMetadata = await setMetadata(pdf, {
+      title: "PDF Title",
+      author: "PDF Author",
+      subject: "PDF Subject",
+      keywords: ["Keyword1", "Keyword2"],
+      producer: "PDF Producer",
+      creator: "PDF Creator",
+      creationDate: new Date(),
+      modificationDate: new Date(),
     });
 
-    doc.pipe(createWriteStream(outputEncryptedPdfPath));
+    // PDFを保存
+    await promises.writeFile(outputPdfPath, pdfWithMetadata);
 
-    doc.addContent(pdf);
+    // PDFを暗号化
+    await encryptPdf(outputPdfPath, outputEncryptedPdfPath, "owner-password");
 
-    doc.end();
-
-    // PDFメタデータを設定
-    // const pdfWithMetadata = await setMetadata(pdf, {
-    //   title: "PDF Title",
-    //   author: "PDF Author",
-    //   subject: "PDF Subject",
-    //   keywords: ["Keyword1", "Keyword2"],
-    //   producer: "PDF Producer",
-    //   creator: "PDF Creator",
-    //   creationDate: new Date(),
-    //   modificationDate: new Date(),
-    // });
-
-    // // PDFを保存
-    // await promises.writeFile(outputPdfPath, pdfWithMetadata);
-
-    // // PDFを暗号化
-    // await encryptPdf(outputPdfPath, outputEncryptedPdfPath, "owner-password");
-
-    // console.log("PDFが生成されました");
+    console.log("PDFが生成されました");
 
     // PDFバッファを読み込む
     const pdfBuffer = await promises.readFile(outputEncryptedPdfPath);
@@ -148,7 +118,7 @@ export const generatePdfFactory = (browser: Browser) => async (url: string) => {
     // リソースのクリーンアップ
     await Promise.all([
       page.close(),
-      // promises.unlink(outputPdfPath).catch(() => {}),
+      promises.unlink(outputPdfPath).catch(() => {}),
       promises.unlink(outputEncryptedPdfPath).catch(() => {}),
     ]);
   }
