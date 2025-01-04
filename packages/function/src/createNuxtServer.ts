b2a799c4009b91fb741a0a4eb4c5cf9e2aa911b0
ytk6565 <ytk6565@gmail.com>
2025-01-04 20:09:23 +0900

    型定義を修正

--- packages/function/src/createNuxtServer.ts
+++ packages/function/src/createNuxtServer.ts
@@ -1,4 +1,4 @@
-import type { ExecaChildProcess } from "execa";
+import type { ResultPromise } from "execa";
 
 import { execaNode } from "execa";
 
@@ -33,7 +33,7 @@ export const createNuxtServer: CreateNuxtServer = ({
 } = {}) => {
   const command = `${serverFilePath}`;
 
-  let execProcess: ExecaChildProcess | undefined;
+  let execProcess: ResultPromise | undefined;
 
   const close = () => {
     execProcess?.kill();
