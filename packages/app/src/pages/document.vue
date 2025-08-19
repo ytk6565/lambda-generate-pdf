4e75625fecb358576327daa7b3ea6f25a03cf6b6
ytk6565 <ytk6565@gmail.com>
2025-08-20 08:33:44 +0900

    画像を表示するサンプルを追加する

--- packages/app/src/pages/document.vue
+++ packages/app/src/pages/document.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
 import { useRoute } from "#vue-router";
 
+import assets from "~/assets/assets.png";
+
 const route = useRoute();
 </script>
 
 <template>
   <div>
     <p>{{ route.query.message }} from query.</p>
+
+    <h2>Public</h2>
+    <img src="/public.png" alt="placeholder" width="120" height="80" />
+
+    <h2>Assets</h2>
+    <img :src="assets" alt="placeholder" width="120" height="80" />
   </div>
 </template>
